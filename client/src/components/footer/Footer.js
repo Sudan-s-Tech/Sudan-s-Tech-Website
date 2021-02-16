62a40877e06299576d0311ce55b34902d711a216
ampsteric <ampsteric@gmail.com>
2021-02-17 00:09:03 +0530

    change footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -6,24 +6,74 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import Logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 export default function Discord() {
     return (
         <div style={{ padding: 50, backgroundColor: "#212121" }}>
             <div className="container">
-                <div className="row d-flex justify-content-center my-5">
-                    <div className="col-sm-1" style={{ color: "#E7497B" }}>
-                        <span>
-                            <InstagramIcon />
-                        </span>
+                <div className="row" align="center">
+                    <div className="col-md-3">
+                        <img src={Logo} style={{ width: "70%" }} />
                     </div>
-                    <div className="col-sm-1" style={{ color: "#0A66C2" }}>
-                        <LinkedInIcon />
+                    <div className="col-md-3" style={{ marginTop: 20 }}>
+                        <ul style={{ textAlign: "center" }}>
+                            <li>
+                                <Link>Home</Link>
+                            </li>
+                            <li>
+                                <Link>Event</Link>
+                            </li>
+                            <li>
+                                <Link>Trainings</Link>
+                            </li>
+                        </ul>
                     </div>
-                    <div className="col-sm-1" style={{ color: "#1199F6" }}>
-                        <FacebookIcon />
+                    <div className="col-md-3" style={{ marginTop: 20 }}>
+                        <ul style={{ textAlign: "center" }}>
+                            <li>
+                                <Link>Contact us</Link>
+                            </li>
+                            <li>
+                                <Link>Sponsors</Link>
+                            </li>
+                            <li>
+                                <Link>Team</Link>
+                            </li>
+                        </ul>
                     </div>
-                    <div className="col-sm-1" style={{ color: "#1A91DA" }}>
-                        <TwitterIcon />
+                    <div className="col-md-3">
+                        <div className="container">
+                            {" "}
+                            <div className="row d-flex justify-content-center my-5">
+                                <div
+                                    className="col-sm-3"
+                                    style={{ color: "#E7497B" }}
+                                >
+                                    <span>
+                                        <InstagramIcon />
+                                    </span>
+                                </div>
+                                <div
+                                    className="col-sm-3"
+                                    style={{ color: "#0A66C2" }}
+                                >
+                                    <LinkedInIcon />
+                                </div>
+                                <div
+                                    className="col-sm-3"
+                                    style={{ color: "#1199F6" }}
+                                >
+                                    <FacebookIcon />
+                                </div>
+                                <div
+                                    className="col-sm-3"
+                                    style={{ color: "#1A91DA" }}
+                                >
+                                    <TwitterIcon />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
